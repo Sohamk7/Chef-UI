47d92bc4a866183aa7ba07834ab7d37188582d86
muktasalunke-hash <69347246+muktasalunke-hash@users.noreply.github.com>
2021-05-10 11:56:04 +0530

    permission added for admin and chefs

--- Chef-Portal-UI/src/app/main/admin/dashboard/dashboard.component.ts
+++ Chef-Portal-UI/src/app/main/admin/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,12 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
   
-  userType: any;
-  constructor() {
-    this.userType = localStorage.getItem('userType') ? localStorage.getItem('userType') :'';
+  checkUserType: boolean = false;
+  
+  constructor(
+    private router:Router
+  ) {
+    let userType = localStorage.getItem('userType');
+    this.checkUserType = userType === 'true' ? true : false;
+    console.log(this.checkUserType);
    }
 
   ngOnInit(): void {
   }
+
+  logout() {
+        
+    //SHOW LOADER BAR #EXTRA Changes
+
+    // const token = this.currentUserValue.token;
+    // //remove from database 
+    // return this.http.post<any>(`${this.appConfig.url.apiUrl}auth/logout`, { token })
+    //     .pipe(map(logoutResponse => {
+    //         // login successful if there's a jwt token in the response
+          
+    //         this._fuseProgressBarService.hide();
+    //         if (logoutResponse) {
+    //             // store loginResponse details and jwt token in local storage to keep loginResponse logged in between page refreshes
+    //             localStorage.removeItem('token');
+    //             localStorage.removeItem('settings');
+    //             localStorage.removeItem('themesettings');
+                localStorage.removeItem('userType');
+                this.router.navigate(['/']);
+    //             this.currentUserSubject.next(null);
+    //         }
+    //         return logoutResponse;
+    //     }),catchError(err => { return throwError("Error in logout");}));
+
+
+
+    // this.currentUserSubject.next(null);
+}
 
 }
