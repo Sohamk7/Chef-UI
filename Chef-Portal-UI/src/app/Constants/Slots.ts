094364f1fda1db52a2d089eac67ca8dc6e98c235
muktasalunke-hash <69347246+muktasalunke-hash@users.noreply.github.com>
2021-06-01 19:13:14 +0530

    code added

---
+++ Chef-Portal-UI/src/app/Constants/Slots.ts
@@ -0,0 +1,12 @@
+import { Slot } from "../_interface";
+
+export const SLOTS: Slot[] = [
+    {
+        name:'13:00',
+        value:'13'
+    },
+    {
+        name:'14:00',
+        value:'14'
+    }
+]
