e7b55171e23966422c466aaadb0075d8d9ab8913
sagarwadgure <sagarw.avertatech@gmail.com>
2021-07-12 18:06:27 +0530

    chef menu design

---
+++ Chef-Portal-UI/src/app/main/admin/chef-payment/chef-payment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chef-payment',
+  templateUrl: './chef-payment.component.html',
+  styleUrls: ['./chef-payment.component.scss']
+})
+export class ChefPaymentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
