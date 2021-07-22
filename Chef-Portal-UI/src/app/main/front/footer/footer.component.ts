75df7a5c811e9656ada531e36938a645b03be22c
suyog moon <suyogmoon8@gmail.com>
2021-07-22 09:59:17 +0530

    landing_page_changes

--- Chef-Portal-UI/src/app/main/front/footer/footer.component.ts
+++ Chef-Portal-UI/src/app/main/front/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+  routerLink= "auth/login"
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onClick(){
+    return this.routerLink;
   }
 
 }
--- Chef-Portal-UI/src/app/main/front/footer/footer.module.ts
+++ Chef-Portal-UI/src/app/main/front/footer/footer.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer.component';
 import { MaterialModule } from '../../material.module';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
@@ -10,7 +11,8 @@ import { MaterialModule } from '../../material.module';
   ],
   imports: [
     CommonModule,
-    MaterialModule
+    MaterialModule,
+    RouterModule
   ],
   exports: [
     FooterComponent
